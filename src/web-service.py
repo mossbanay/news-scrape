f8654735c4ec94f09a3a37c184fb622cab8b4d5f
Moss Banay <mossbanay@gmail.com>
2015-03-24 15:39:41 +1100

    Implemented a basic word cloud
    Using the d3-cloud library by Jason Davies, implemented a word cloud
    that uses the current headlines to produce a nice visual.

--- src/web-service.py
+++ src/web-service.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template
+import itertools
 import scrape
 import sqlite3
 
@@ -6,13 +7,28 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    latest = get_latest_headlines()
-    
-    return render_template('home.html', headlines = latest['headlines'])
+    return render_template('home.html')
+
+@app.route('/cloud', methods=['GET'])
+def words():
+    words = []
+
+    headlines = get_current_headlines();
+
+    details = [h['detail'] for h in headlines['headlines']]
+    details = [h.lower().split() for h in details]
+    details = list(itertools.chain(*details)) 
+
+    return render_template('cloud.html', words=details)
 
 @app.route('/scrape/api/v1.0/latest', methods=['GET'])
 def latest():
     result = get_latest_headlines()
+    return jsonify(result)
+
+@app.route('/scrape/api/v1.0/current', methods=['GET'])
+def current():
+    result = get_current_headlines()
     return jsonify(result)
 
 def get_latest_headlines():
@@ -23,6 +39,15 @@ def get_latest_headlines():
     result = {'headlines': aus+hs}
 
     return result 
+
+def get_current_headlines():
+    # Go get the latest from each newspaper
+    aus = scrape.get_australian_headlines()
+    hs = scrape.get_herald_sun_headlines()
+    
+    result = {'headlines': aus+hs}
+
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
