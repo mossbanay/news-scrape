cf9ba133907be0d723131b009f46141d1be97614
Moss Banay <mossbanay@gmail.com>
2015-03-25 14:39:28 +1100

    Update update-database.py
    Updated update-database.py to work with database.py

--- src/update-database.py
+++ src/update-database.py
@@ -1,13 +1,14 @@
 import scrape
+import database
 import os
 
 # Check if db exists
 if os.path.isfile('headlines.db') == False:
-    scrape.create_db('headlines.db')
+    database.create_db('headlines.db')
 
 # Update db
 aus = scrape.get_australian_headlines()
 hs = scrape.get_herald_sun_headlines()
 
-scrape.save_to_db('headlines.db', aus)
-scrape.save_to_db('headlines.db', hs)
+database.save_to_db('headlines.db', aus)
+database.save_to_db('headlines.db', hs)
